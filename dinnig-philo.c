8380cb65a6e625a239bb8272244158cd5c012509
shebin007 <shebinkp7@gmail.com>
2018-11-24 22:54:53 +0530

    dinnig philospher semaphore

--- dinnig-philo.c
+++ dinnig-philo.c
@@ -62,4 +62,64 @@ void take_fork(int phnum)
 } 
   
 // put down chopsticks 
-
+void put_fork(int phnum) 
+{ 
+  
+    sem_wait(&mutex); 
+  
+    // state that thinking 
+    state[phnum] = THINKING; 
+  
+    printf("Philosopher %d putting fork %d and %d down\n", 
+           phnum + 1, LEFT + 1, phnum + 1); 
+    printf("Philosopher %d is thinking\n", phnum + 1); 
+  
+    test(LEFT); 
+    test(RIGHT); 
+  
+    sem_post(&mutex); 
+} 
+  
+void* philospher(void* num) 
+{ 
+  
+    while (1) { 
+  
+        int* i = num; 
+  
+        sleep(1); 
+  
+        take_fork(*i); 
+  
+        sleep(0); 
+  
+        put_fork(*i); 
+    } 
+} 
+  
+int main() 
+{ 
+  
+    int i; 
+    pthread_t thread_id[N]; 
+  
+    // initialize the semaphores 
+    sem_init(&mutex, 0, 1); 
+  
+    for (i = 0; i < N; i++) 
+  
+        sem_init(&S[i], 0, 0); 
+  
+    for (i = 0; i < N; i++) { 
+  
+        // create philosopher processes 
+        pthread_create(&thread_id[i], NULL, 
+                       philospher, &phil[i]); 
+  
+        printf("Philosopher %d is thinking\n", i + 1); 
+    } 
+  
+    for (i = 0; i < N; i++) 
+  
+        pthread_join(thread_id[i], NULL); 
+} 
